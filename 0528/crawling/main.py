4b68e762ab74c26049286eff9ee2d4c121e8593c
doorBW <doorbw@outlook.com>
2018-06-01 18:27:16 +0900

    0528 class

--- 0528/crawling/main.py
+++ 0528/crawling/main.py
@@ -1,24 +1,34 @@
+# 필요한 라이브러리를 우측 명령어를 통해 설치하여 import 한다.
 import requests # pip install requests 
 from bs4 import BeautifulSoup # pip install bs4
 
+# flask 프로젝트 가져오기
 from flask import Flask, render_template
+# app이라는 이름으로 플라스크 프로젝트 초기화
 app = Flask(__name__)
 
+# / 라는 url로 들어오게 되면
 @app.route('/')
+# hello_wolrd()함수를 실행한다.
 def hello_world():
-    # HTTP GET Request
+    # HTTP GET Request, 입력한 url에 대한 정보를 가져온다.
     req = requests.get('http://uos.ac.kr/korNotice/list.do?list_id=FA1')
-    # HTML 소스 가져오기
+    # HTML 소스 가져오기, req에 존재하는 text(html code)를 가져온다.
     html = req.text
-    # HTTP Header 가져오기
+    # HTTP Header 가져오기, req에 있는 headers를 가져온다. 현재 우리에겐 필요 x
     header = req.headers
-    # HTTP Status 가져오기 (200: 정상)
+    # HTTP Status 가져오기 (200: 정상), status 확인, 현재 우리에겐 필요 x
     status = req.status_code
-    # HTTP가 정상적으로 되었는지 (True/False)
+    # HTTP가 정상적으로 되었는지 (True/False), 정상적으로 접근이 안된다면 위에서 가져온 html코드가 잘못된 것일 수 있으므로
+    # 이를 체크하여, 정상적으로 접근되었을때를 처리한다.
     is_ok = req.ok
-    if is_ok:
+    if is_ok: # is_ok, 즉 정상접근이 Ture라면
+        # bs를 이용하여 우리가 가져온 html코드를 파싱한다(나눈다.) 왜? 우리가 보고 싶은 부분만 보기 위해서
         soup = BeautifulSoup(html, 'html.parser')
+        # 우리는 학교 공지사항에서 각 제목만 가져올것, 개발자도구에서 가져오고자 하는 요소의 코드에서 우클릭하여 copy selector를 누르면된다.
+        # 이를 통해 걸러낸 요소를 리스트로 반환한다. 즉, title = [제목1, 제목2, ...] 이런식으로 저장될 것
         title = soup.select('#contents > ul > li > a')
+        # render_template 함수를 이용하여 main.html을 반환하고 이때 titles라는 이름의 변수로 우리가 가져온 리스트 title을 넘겨준다.
         return render_template('main.html', titles = title)
     else:
         return '가져오기 실패'
--- 0528/gugudan/gugudan.py
+++ 0528/gugudan/gugudan.py
@@ -1,17 +1,30 @@
+# flask를 비롯해 필요한 함수들을 import 한다.
 from flask import Flask, render_template, request, redirect, url_for
+# app이란 이름으로 flask 프로젝트 초기화
 app = Flask(__name__)
 
+# / 이란 주소와 /<특정값> 이라는 주소에 접근하면,
 @app.route('/')
 @app.route('/<url_answer>')
+# hello_world 함수를 실행하는데, 이때 url_answer의 기본값은 None이다.
 def hello_world(url_answer=None):
+    # answer라는 값에 url_answer로 받은 값을 준다.
     answer = url_answer
+    # render_template 함수를 이용하여 gu_main.html 파일을 연다.
+    # 이때, html에서 사용하기 위해 render_answer라는 변수 이름으로 answer값을 준다.
     return render_template('gu_main.html',render_answer = answer)
 
+# /calculate 라는 주소에 접근하면,
 @app.route('/calculate')
+# calculate 함수를 실행한다.
 def calculate():
+    # html에서 get방식으로 넘어온 것들 중 Input1이름을 가진친구를 input_data1 으로 받는다.
     input_data1 = request.args.get('input1')
+    # html에서 get방식으로 넘어온 것들 중 Input2이름을 가진친구를 input_data2 으로 받는다.
     input_data2 = request.args.get('input2')
+    # answer 라는 변수에 위에서 받은 두 값을 int(정수형)으로 바꾸어서 곱한다.
     answer = int(input_data1) * int(input_data2)
+    # 답을 str(문자열)로 변환시켜주고 url에 붙여서 redirect함수로 전달한다.
     next_url = '/'+str(answer)
     return redirect(next_url)
 
